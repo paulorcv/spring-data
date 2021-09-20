5ff2a41893e2dd16d6e54122981911b97430e492
Paulo Roberto Campos de Vasconcelos <paulorcv@gmail.com>
2021-09-20 20:39:39 -0300

    Interação via console pra criar cargo

--- src/main/java/br/com/asty/springdata/SpringDataApplication.java
+++ src/main/java/br/com/asty/springdata/SpringDataApplication.java
@@ -1,19 +1,23 @@
 package br.com.asty.springdata;
 
+import java.util.Scanner;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.asty.springdata.orm.Cargo;
 import br.com.asty.springdata.repository.CargoRepository;
+import br.com.asty.springdata.service.CrudCargoService;
 
 @SpringBootApplication
 public class SpringDataApplication implements CommandLineRunner{
 
-	private final CargoRepository repository;
+	private final CrudCargoService cargoService;
+	private boolean system = true;
 
-	public SpringDataApplication(CargoRepository repository){
-		 this.repository = repository;
+	public SpringDataApplication(CrudCargoService service){
+		 this.cargoService = service;
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
@@ -21,9 +25,21 @@ public class SpringDataApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		Cargo cargo = new Cargo();
-		cargo.setDescricao("DESENVOLVEDOR DE SOFTWARE");
-		repository.save(cargo);
+		Scanner scanner = new Scanner(System.in);
+		
+		while(system){
+			System.out.println("--- Qual ação você quer executar");
+			System.out.println("0 - Sair");
+			System.out.println("1 - Cargo");
+
+			int action = scanner.nextInt();
+			if(action == 1){
+				cargoService.inicial(scanner);
+			}else{
+				system = false;
+			}
+		}
+
 	}
 
 	
