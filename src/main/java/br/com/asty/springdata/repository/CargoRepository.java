569cda7586fce33a5f7ac50b02385f1b6d37d7cb
Paulo Roberto Campos de Vasconcelos <paulorcv@gmail.com>
2021-09-20 20:08:42 -0300

    Criação do repositório de Cargo

---
+++ src/main/java/br/com/asty/springdata/repository/CargoRepository.java
@@ -0,0 +1,9 @@
+package br.com.asty.springdata.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CargoRepository extends CrudRepository{
+    
+}
