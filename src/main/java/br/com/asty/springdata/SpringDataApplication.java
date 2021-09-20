9958fb00be49a94cdd3e891c50557d8f7b602ac8
Paulo Roberto Campos de Vasconcelos <paulorcv@gmail.com>
2021-09-20 20:20:39 -0300

    Chamar repositório

--- src/main/java/br/com/asty/springdata/SpringDataApplication.java
+++ src/main/java/br/com/asty/springdata/SpringDataApplication.java
@@ -1,13 +1,31 @@
 package br.com.asty.springdata;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringDataApplication {
+import br.com.asty.springdata.orm.Cargo;
+import br.com.asty.springdata.repository.CargoRepository;
 
+@SpringBootApplication
+public class SpringDataApplication implements CommandLineRunner{
+
+	private final CargoRepository repository;
+
+	public SpringDataApplication(CargoRepository repository){
+		 this.repository = repository;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Cargo cargo = new Cargo();
+		cargo.setDescricao("DESENVOLVEDOR DE SOFTWARE");
+		repository.save(cargo);
+	}
+
+	
 
 }
--- src/main/java/br/com/asty/springdata/repository/CargoRepository.java
+++ src/main/java/br/com/asty/springdata/repository/CargoRepository.java
@@ -3,7 +3,9 @@ package br.com.asty.springdata.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import br.com.asty.springdata.orm.Cargo;
+
 @Repository
-public interface CargoRepository extends CrudRepository{
+public interface CargoRepository extends CrudRepository<Cargo, Integer>{
     
 }
