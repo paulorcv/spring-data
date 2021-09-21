e88911eedf5f661f28994880481938b7b5af43b8
Paulo Roberto Campos de Vasconcelos <paulorcv@gmail.com>
2021-09-20 21:12:28 -0300

    Métodos de listar e apagar

--- src/main/java/br/com/asty/springdata/orm/Cargo.java
+++ src/main/java/br/com/asty/springdata/orm/Cargo.java
@@ -27,6 +27,10 @@ public class Cargo {
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
+    @Override
+    public String toString() {
+        return "Cargo [id=" + id + ", descricao=" + descricao + "]";
+    }
 
     
 
--- src/main/java/br/com/asty/springdata/service/CrudCargoService.java
+++ src/main/java/br/com/asty/springdata/service/CrudCargoService.java
@@ -1,5 +1,6 @@
 package br.com.asty.springdata.service;
 
+import java.util.List;
 import java.util.Scanner;
 
 import org.springframework.stereotype.Service;
@@ -22,8 +23,10 @@ public class CrudCargoService {
         while(system){
             System.out.println("Qual ação de cargo deseja executar?");
             System.out.println("0 - Sair");
-            System.out.println("1 - Salvar");
+            System.out.println("1 - Inserir");
             System.out.println("2 - Atualizar");
+            System.out.println("3 - Listar");
+            System.out.println("4 - Apagar");
 
             int action = scanner.nextInt();
 
@@ -36,6 +39,15 @@ public class CrudCargoService {
                 case 2:
                     atualizar(scanner);
                     break;
+
+                case 3:
+                    listar();
+                    break;
+
+                case 4:
+                    apagar(scanner);
+                    break;
+
 
                 default:
                     system = false;
@@ -66,5 +78,27 @@ public class CrudCargoService {
         System.out.println("Atualizado");
     }
     
+    private void apagar(Scanner scanner){
+        System.out.println("Id");
+        int id = scanner.nextInt();
+
+        Cargo cargo = new Cargo();
+        cargo.setId(id);
+        cargoRepository.delete(cargo);
+        System.out.println("Apagado");
+    }
+
+    private void listar(){
+        System.out.println("Lista de cargos:");
+        
+        List<Cargo> lista = (List<Cargo>) cargoRepository.findAll();
+
+        for (Cargo cargo : lista) {
+            System.out.println(cargo);
+        }
+
+        System.out.println("-------------");
+    }
+
 
 }
